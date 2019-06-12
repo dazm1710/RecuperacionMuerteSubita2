19eb04a361472c9951d281b351769790660ed395
Alejandro <42217598+dazm1710@users.noreply.github.com>
2019-06-12 18:03:54 -0500

    Add files via upload

--- recuperacion.py
+++ recuperacion.py
@@ -1,7 +1,7 @@
 #1
 
 print "Primero:"
-items =[[12,23,34,56],[10,23,34,33],[11,23,34,44],[11,23,34,66]]
+items =[[12,23,34,56],[10,23,34,33],[11,23,22,34,44],[11,23,34,66]]
 print map(lambda x: [x[0],x[len(x)-1]],items)
 
 #2
@@ -19,10 +19,12 @@ print map(lambda x: max(x),items)
 #4
 
 print "\nCuarto:"
-print map(lambda x: min(x),items)
+print map(lambda k: map(lambda k: k[1],filter(lambda o: o[0]==True,zip(map(lambda z: (z==[]),map(lambda x: filter(lambda x: x%2!=0,map(lambda x: int(x),(list(filter(lambda x: x!="[" or x!="]",str(x)))))),k)),k))),items)
+z =map(lambda b: b,filter(lambda i: i!=[],map(lambda k: map(lambda k: k[1],filter(lambda o: o[0]==True,zip(map(lambda z: (z==[]),map(lambda x: filter(lambda x: x%2!=0,map(lambda x: int(x),(list(filter(lambda x: x!="[" or x!="]",str(x)))))),k)),k))),items)))
+print z
 
-#print map(lambda k: k[1],filter(lambda o: o[0]==True,zip(map(lambda z: (z==[]),map(lambda x: filter(lambda x: x%2!=0,map(lambda x: int(x),(list(filter(lambda x: x!="[" or x!="]",str(x)))))),lista)),lista)))
-#print map(lambda x: min(x),map(lambda k: k[1],filter(lambda o: o[0]==True,zip(map(lambda z: (z==[]),map(lambda x: filter(lambda x: x%2!=0,map(lambda x: int(x),(list(filter(lambda x: x!="[" or x!="]",str(x)))))),items)),items))))
+#print map(lambda x: min(x),z)
+
 #5
 
 print "\nQuinto:"
@@ -32,18 +34,12 @@ print filter (lambda x: x<lista5[0]**5,lista5)
 
 #6
 print "\nSexto:"
+        
 class tupla:
     def __init__(self, valorx, valory):
         self.valorx = valorx
         self.valory = valory
 
 tuplas=[tupla(6,3), tupla(15,5), tupla(234,7), tupla(21,6)]
-print 
 
-#T(n) : n*(n+1)/2
-
-
-
-print reduce(lambda x,y: x+y, map(lambda w:w.valorx,
-                                  filter(lambda z: z.valorx==(z.valory*(z.valory+1))/2,
-                                         [tupla(1,1), tupla(5,2), tupla(15,5), tupla(234,7), tupla(21,6)])))
+print reduce(lambda x,y: x+y, map(lambda y: y.valorx,filter(lambda u: u.valorx==u.valory*(u.valory+1)/2,tuplas)))
